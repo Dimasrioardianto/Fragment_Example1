202bfd548f6db4b46f11abf0ec6f31220e4cafbe
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-10-28 14:06:13 +0700

    Update Commit 2 Fragment

---
+++ app/src/main/java/id/ac/polbeng/ardianto/fragmentexample1/Coordinator.kt
@@ -0,0 +1,5 @@
+package id.ac.polbeng.ardianto.fragmentexample1
+
+interface Coordinator {
+    fun onBookChanged(index:Int)
+}
--- app/src/main/java/id/ac/polbeng/ardianto/fragmentexample1/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/ardianto/fragmentexample1/MainActivity.kt
@@ -1,20 +1,23 @@
 package id.ac.polbeng.ardianto.fragmentexample1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import id.ac.polbeng.ardianto.fragmentexample1.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Coordinator {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         }
+
+    override fun onBookChanged(index: Int) {
+        val frag = supportFragmentManager.findFragmentById(R.id.fragment_book_description)
+        if (frag is BookDescription) {
+            frag.changeDescription(index)
+    }
     }
 }
